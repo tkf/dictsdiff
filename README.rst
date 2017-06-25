f312ed0a3a3411729460de3f6f1e9167bb3a2953
Takafumi Arakaki <aka.tkf@gmail.com>
2017-06-24 19:36:53 -0700

    Add files from my template

---
+++ README.rst
@@ -0,0 +1,2 @@
+dictsdiff package
+=================
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dictsdiff',
+    version="0.0.0",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    author='Takafumi Arakaki',
+    author_email='aka.tkf@gmail.com',
+    # url='https://github.com/tkf/dictsdiff',
+    license='BSD-2-Clause',  # SPDX short identifier
+    # description='dictsdiff - THIS DOES WHAT',
+    long_description=open('README.rst').read(),
+    # keywords='KEYWORD, KEYWORD, KEYWORD',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: BSD License',
+        # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    install_requires=[
+        # 'SOME_PACKAGE',
+    ],
+    # entry_points={
+    #     'console_scripts': ['PROGRAM_NAME = dictsdiff.cli:main'],
+    # },
+)
