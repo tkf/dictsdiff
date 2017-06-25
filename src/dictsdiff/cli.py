6c211bce96eaca17de770b82aab8dac07ff0c2fd
Takafumi Arakaki <aka.tkf@gmail.com>
2017-06-25 00:04:36 -0700

    Set display.width based on terminal size

--- src/dictsdiff/cli.py
+++ src/dictsdiff/cli.py
@@ -6,6 +6,15 @@ from __future__ import print_function
 
 import sys
 
+try:
+    from shutil import get_terminal_size
+except ImportError:
+    def get_terminal_size():
+        from subprocess import check_output
+        out = check_output(['stty', 'size'], universal_newlines=True)
+        rows, columns = map(int, out.strip().split())
+        return columns, rows
+
 
 def dictsdiff_cli(files):
     import pandas
@@ -16,8 +25,14 @@ def dictsdiff_cli(files):
     else:
         dd = diff_ndjson(sys.stdin)
 
+    # Manually detect terminal size, since passing "'display.width',
+    # None" does not detect terminal size (as advertised in
+    # https://pandas.pydata.org/pandas-docs/stable/options.html):
+    width, _ = get_terminal_size()
+
     with pandas.option_context('display.max_rows', None,
-                               'display.max_columns', None):
+                               'display.max_columns', None,
+                               'display.width', width):
         print(dd.pretty_diff())
 
 
