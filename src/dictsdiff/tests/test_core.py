8ca799d635d3576de87ce2dffe1f632e9bce34a1
Takafumi Arakaki <aka.tkf@gmail.com>
2018-05-24 23:14:06 -0700

    More tests for core

--- src/dictsdiff/tests/test_core.py
+++ src/dictsdiff/tests/test_core.py
@@ -4,6 +4,7 @@ from ..core import DictsDiff, diff_dicts
 
 
 @pytest.mark.parametrize('value_dicts, diff_keys', [
+    ([dict(a=1)], []),
     ([dict(a=1)] * 2, []),
     ([dict(a=1), dict(a=2)], ['a']),
     ([dict(a=1), dict(b=1)], ['a', 'b']),
@@ -20,6 +21,8 @@ def test_diff_flat_keys(value_dicts, diff_keys):
     ([dict(a=1.0), dict(a=0.999)], ['a'], 0, 0),
     ([dict(a=1.0), dict(a=0.999)], [], 1e-2, 0),
     ([dict(a=1.0), dict(a=0.999)], [], 0, 1e-2),
+    ([dict(a=1.0), dict(a=0.999)], ['a'], 1e-6, 0),
+    ([dict(a=1.0), dict(a=0.999)], ['a'], 0, 1e-6),
 ])
 def test_float_diff_with_tol(value_dicts, diff_keys, rtol, atol):
     dd = diff_dicts(value_dicts, rtol=rtol, atol=atol)
