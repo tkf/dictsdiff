86bbd22770a10f2ac8f050b156265a502a7d7843
Takafumi Arakaki <aka.tkf@gmail.com>
2019-02-25 15:16:09 -0800

    Bump to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dictsdiff',
-    version="0.1.0.dev1",
+    version="0.1.0",
     packages=find_packages('src'),
     package_dir={'': 'src'},
     author='Takafumi Arakaki',
