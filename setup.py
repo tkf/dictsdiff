94a40c47b542975143e125883f459a90eb3c3cf1
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-12 23:22:23 -0800

    Stop testing with Python 3.5

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
