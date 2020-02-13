1bb27cf0b657d30d6bdc67e9b6a8710060359550
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-12 22:53:29 -0800

    Test with Python 3.7/3.8 and Pandas 1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,10 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development',
         # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
