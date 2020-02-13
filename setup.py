a8513fc595e14465038d1839470cbe55e2065c95
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-12 23:31:09 -0800

    Test with Python 3.7/3.8 and Pandas 1.0 (#3)
    * Test with Python 3.7/3.8 and Pandas 1.0
    
    * Test force_tuple_columns with Python 3.6
    
    * Stop testing with Python 3.5

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development',
         # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
