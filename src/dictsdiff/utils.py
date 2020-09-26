c1b9ad7d0d4222d4bfb5e9e97fba16028ceea043
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-25 20:21:41 -0700

    Remove get_terminal_size_stty

--- src/dictsdiff/utils.py
+++
@@ -1,8 +0,0 @@
-from subprocess import check_output
-
-
-def get_terminal_size_stty():
-    with open('/dev/tty') as stdin:
-        out = check_output(['stty', 'size'], stdin=stdin)
-    rows, columns = map(int, out.decode().strip().split())
-    return columns, rows
